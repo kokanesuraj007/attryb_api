6a577df9e3c2f50d6ec54f91d994e28ad034498c
suraj sanjay kokane <108039887+kokanesuraj007@users.noreply.github.com>
2023-07-08 11:35:56 +0530

    api

--- config/db.js
+++ config/db.js
@@ -1,5 +1,16 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
-const DBURL = "mongodb+srv://sk:sk@cluster0.pwxhqpd.mongodb.net/?retryWrites=true&w=majority";
-const connection=mongoose.connect(DBURL);
-module.exports={connection}; 
+// const DBURL = "mongodb+srv://sk:sk@cluster0.pwxhqpd.mongodb.net/?retryWrites=true&w=majority";
+const connection = async () =>{
+    try {
+        await mongoose.connect(process.env.DBURL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1);
+    }
+}
+module.exports= {connection} ;
--- config/dist/db.dev.js
+++ config/dist/db.dev.js
@@ -2,10 +2,40 @@
 
 var mongoose = require("mongoose");
 
-require("dotenv").config();
+require("dotenv").config(); // const DBURL = "mongodb+srv://sk:sk@cluster0.pwxhqpd.mongodb.net/?retryWrites=true&w=majority";
 
-var DBURL = "mongodb+srv://sk:sk@cluster0.pwxhqpd.mongodb.net/?retryWrites=true&w=majority";
-var connection = mongoose.connect(DBURL);
+
+var connection = function connection() {
+  return regeneratorRuntime.async(function connection$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.prev = 0;
+          _context.next = 3;
+          return regeneratorRuntime.awrap(mongoose.connect(process.env.DBURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+          }));
+
+        case 3:
+          console.log('Connected to MongoDB');
+          _context.next = 10;
+          break;
+
+        case 6:
+          _context.prev = 6;
+          _context.t0 = _context["catch"](0);
+          console.error('Error connecting to MongoDB:', _context.t0.message);
+          process.exit(1);
+
+        case 10:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, null, [[0, 6]]);
+};
+
 module.exports = {
   connection: connection
 };
